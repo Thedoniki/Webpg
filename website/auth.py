ac2b188814156bf008c0861607beb41ea62c7bfe
TheDoniki <donikamehmetiprotonmail.com>
2026-02-28 07:36:09 +0100

    generated from my other github and started up on my desktop

--- website/auth.py
+++ website/auth.py
@@ -1,27 +1,62 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, flash, redirect, render_template, request, url_for
 
 # "Blueprint" of application -> a way to organize a group of related views and other code.
-
 
 auth = Blueprint('auth', __name__)
 
 
-@auth.route('/login')  # login page
+@auth.route('/login', methods=['GET', 'POST'])  # login page
 def login():
-	#   return render_template('login.html', text="Test", user="Donika")
-	
-	#	return render_template('login.html', boolean=True) # if statment in login.html will be true and show the text
-	#	"You are logged in" instead of "You are not logged in"
-	
-	return render_template('login.html')
+    """
+    Just some code snippets for reference:
+    return render_template("login.html", text="Test", user="Donika")
+    return render_template("login.html", boolean=True) # if statement in login.html will be true and show the text
+    You are logged in" instead of "You are not logged in"
+    """
+
+    if request.method == 'POST':
+        data = request.form
+        email = data.get('email')
+        password = data.get('password')
+
+    # TODO: Add email and password validation
+    # TODO: Check credentials against database
+    # TODO: Implement session management
+
+    return render_template('login.html')
 
 
-@auth.route('/logout')  # logout page
+@auth.route('/logout')
 def logout():
-	return "<p> Logout </p>"
+    # TODO: Clear session/logout user
+    return redirect(url_for('view.home'))
 
 
-@auth.route('/sign-up')  # sign up page
+@auth.route('/sign-up', methods=['GET', 'POST'])
 def sign_up():
-	# return "<p> Sign Up </p>"
-	return render_template('sign_up.html')
+    if request.method == 'POST':
+        email = request.form.get('email')
+        firstName = request.form.get('firstName')
+        password1 = request.form.get('password1')
+        password2 = request.form.get('password2')
+
+        if len(email) < 5:
+            flash('Email must be at least 5 characters long.', category='error')
+        elif len(firstName) < 2:
+            flash('First name must be at least 2 characters long.', category='error')
+        elif password1 != password2:
+            flash('Passwords do not match.', category='error')
+        elif len(password1) < 7:
+            flash('Password must be at least 7 characters long.', category='error')
+        else:
+            flash("Account created successfully!", category='success')
+            # add user to database
+            return redirect(url_for('view.home'))
+
+    return render_template('sign_up.html')
+    
+    
+    ''' Just some code snippets for reference:
+		#data = request.form
+		#email = data.get('email')
+		#password = data.get('password') '''
